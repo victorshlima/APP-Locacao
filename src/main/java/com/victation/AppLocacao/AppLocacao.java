b11fbe9cb45f8e6a9dc0737f7b03b88a2b6b2c34
Victor Hugo <victorshlima@gmail.com>
2022-08-04 23:52:09 -0300

    initial

---
+++ src/main/java/com/victation/AppLocacao/AppLocacao.java
@@ -0,0 +1,13 @@
+package com.victation.AppLocacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLocacao {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppLocacao.class, args);
+    }
+
+}
